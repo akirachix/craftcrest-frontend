f8bd407fb71d289bfbf845c832aef12d255007b1
studen <jecintawanjirumuhia@gmail.com>
2025-09-26 22:07:23 +0300

    solved conflicts

--- craftcrest/src/app/api/payments/route.ts
+++ craftcrest/src/app/api/payments/route.ts
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
+
 const baseUrl = process.env.BASE_URL  ;
-export async function GET() {
-  try {
-    const response = await fetch(
-      `${baseUrl}/payments/`
-    );
-    const result = await response.json();
-    return new Response(JSON.stringify(result), {
-      status: 200,
-    });
-  } catch (error) {
-    return new Response((error as Error).message, {
-      status: 500,
-    });
-  }
-=======
+
 export async function GET() {
     const baseUrl = process.env.BASE_URL;
     if (!baseUrl) {
@@ -31,5 +17,4 @@ export async function GET() {
             status: 500,
         });
     }
->>>>>>> 46ac5e4a615909039317161fe4b650a90c8e59bf
 }
